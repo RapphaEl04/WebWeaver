4b463eae9fb95ae58c4870cb051f72783ecdcaeb
Rappha-Elzion Bauyon <166001242+RapphaEl04@users.noreply.github.com>
2024-05-27 17:34:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let webWeaver = "<b>web weaver</b>";
+let alchemistOfTheBrowser = "<b>alchemist of the browser</b>";
+let cssCloak = "<b>CSS cloak</b>";
+let javaScriptSpells = "<b>JavaScript spells</b>";
+let debuggingSpectacles = "<b>debugging spectacles</b>";
+let responsiveSea = "<b>responsive sea</b>";
+let seoWhisperer = "<b>SEO whisperer</b>";
+let gitAlchemist = "<b>Git alchemist</b>";
+let webDeveloper = "<b>web developer</b>";
+
+let sentence1 = "<i>In the realm of ones and zeros, there resides a silent sorcerer, a " + webWeaver + " with dreams and pearls.</i>";
+let sentence2 = "<i>Rod, the " + alchemistOfTheBrowser + ", wields his keyboard like a wand, conjuring pages from thin air.<i>";
+let sentence3 = "<i>Next, he dons his " + cssCloak + ", styling elements with flair and grace.</i>";
+let sentence4 = "<i>But wait, the magic deepens still, Rod summons " + javaScriptSpells + ", interactivity blooms at his will.</i>";
+let sentence5 = "<i>Yet, bugs creep in like shadows dark, Rod dons his " + debuggingSpectacles + ", chasing glitches through the code's arc.</i>";
+let sentence6 = "<i>Across screens, devices, and dimensions, Rod sails on the " + responsiveSea + ", adapting layouts with fluidity.</i>";
+let sentence7 = "<i>In the labyrinth of search engines, Rod becomes an " + seoWhisperer + ", crafting pathways to digital wanderers.</i>";
+let sentence8 = "<i>Version control, a mystical potion, Rod brews commits in " + gitAlchemist + "'s cauldron.</i>";
+let sentence9 = "<i>So here's to Rod, the " + webDeveloper + ", a poet of pixels, a composer of clicks.</i>";
+
+let contentDiv = document.getElementById("content");
+
+contentDiv.innerHTML += "<p>" + sentence1 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence2 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence3 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence4 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence5 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence6 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence7 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence8 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence9 + "</p>";
